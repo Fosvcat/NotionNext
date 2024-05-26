fe63b5d4c523860ccd3a71bf78dcf8e8d4afc671
Fosvcat <749643014@qq.com>
2024-05-26 21:29:55 +0800

    更新 InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -42,10 +42,13 @@ export function InfoCard(props) {
                 <div className='flex space-x-3  hover:text-black dark:hover:text-white'>
                     {/* 两个社交按钮 */}
                     <div className='bg-indigo-400 p-2 rounded-full  transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
-                      <Link href='https://fosvcat.top/' target="_blank"><i class="fa-solid fa-circle-user fa-xl"></i></Link>
+                        <Link href='/about'><GlobeAlt className={'w-6 h-6'} /></Link>
                     </div>
                     <div className='bg-indigo-400 p-2 rounded-full w-10 items-center flex justify-center transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
-                      <Link href='https://t.me/Fosvcat' target="_blank"><i class="fa-brands fa-telegram fa-xl"></i></Link>
+                      {siteConfig('HEO_INFO_CARD_URL', null, CONFIG) && <Link href='https://t.me/Fosvcat' target="_blank">
+                          <i class="fa-brands fa-telegram fa-xl"></i>
+                        </Link>
+                        }
                     </div>
                 </div>
                 <MoreButton />
